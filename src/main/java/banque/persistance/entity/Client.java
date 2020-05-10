d593cb511ee45ba02f1fc3b1b060b778ae5c9f52
Exoget <ferchichi.feres@gmail.com>
2020-05-10 13:42:44 +0200

    adding unitTest with encoding problems

--- src/main/java/banque/persistance/entity/Client.java
+++ src/main/java/banque/persistance/entity/Client.java
@@ -2,6 +2,7 @@ package banque.persistance.entity;
 
 
 import org.hibernate.validator.constraints.NotBlank;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -24,8 +25,8 @@ public class Client {
     private String adresse;
 
     @NotBlank
-    @Size(max = 5)
-    @Pattern(regexp = "[0-9]{5}")
+    @Pattern(regexp = "[0-9]{5}",
+            message = "{banque.persistance.entity.codepostal.message}")
     private String codePostal;
 
     @NotBlank
@@ -35,4 +36,60 @@ public class Client {
     @NotBlank
     @Size(max = 100)
     private String motDePasse;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
 }
--- src/test/java/banque/persistance/entity/ClientTest.java
+++ src/test/java/banque/persistance/entity/ClientTest.java
@@ -1,4 +1,65 @@
 package banque.persistance.entity;
 
+
+import org.junit.*;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
 public class ClientTest {
+    private Client client;
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        client = new Client();
+        client.setId(12);
+        client.setNom("FERCHICHI");
+        client.setPrenom("Fares");
+        client.setAdresse("40 rue de la Paix");
+        client.setCodePostal("92000");
+        client.setVille("Nanterre");
+        client.setMotDePasse("secret");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testClient() {
+        client = new Client();
+        assertNotNull(client);
+    }
+
+    @Test
+    public void testCodePostalErrone() {
+
+        client.setCodePostal("440000");
+        assertEquals("440000", client.getCodePostal());
+
+        Set<ConstraintViolation<Client>> violations = validator.validate(client);
+        assertFalse(violations.isEmpty());
+
+        assertEquals(1, violations.size());
+        assertEquals(
+                "Le code postal est erroné.",
+                violations.iterator().next().getMessage()
+        );
+    }
 }
