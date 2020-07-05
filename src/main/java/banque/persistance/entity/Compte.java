3467c8f207721180fd3de05df9d67e5502e3b14d
Exoget <ferchichi.feres@gmail.com>
2020-07-05 13:07:51 +0200

    update structures

--- src/main/java/banque/persistance/entity/Compte.java
+++ src/main/java/banque/persistance/entity/Compte.java
@@ -3,6 +3,7 @@ package banque.persistance.entity;
 
 import javax.persistence.*;
 
+//JPA 2 identifie les objets qui vont être persistés
 @Entity
 @Table(name = "compte")
 @NamedQueries({
@@ -24,6 +25,8 @@ public class Compte {
     @Column(name = "solde", precision = 10, scale = 2)
     private double solde;
 
+    //Les relations entre ces entités sont gérées par des méta-données définies à partir
+    // d’annotations directement dans la classe ou de fichiers XML externes
     @ManyToOne(targetEntity = Client.class, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "idclient")
     private Client client;
