1cbe19ca859312b8e7c541cea8f3fc26de920210
Exoget <ferchichi.feres@gmail.com>
2021-02-20 19:16:46 +0100

    ajouter tests d'intergration

---
+++ banque-ws/banque-webservice/src/test/java/fr/eni/mvnbook/banque/ws/service/BanqueWebServiceImplIT.java
@@ -0,0 +1,16 @@
+package fr.eni.mvnbook.banque.ws.service;
+
+import org.junit.Test;
+
+public class BanqueWebServiceImplIT {
+
+    @Test
+    public void testauthentifier(){
+        // ....
+    }
+
+    @Test
+    public void testMesComptes(){
+        // ....
+    }
+}
